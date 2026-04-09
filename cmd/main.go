2ac4f7a6cd2b77bf6bc311d0be80b698c4441ff3
Dmytro Voron <dmytro.voron@gmail.com>
2026-04-09 17:48:56 +0300

    add generated with go-swagger server

---
+++ cmd/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"errors"
+	"log"
+	"os"
+
+	"github.com/dmytrovoron/github-release-notification/internal/http/restapi"
+	"github.com/dmytrovoron/github-release-notification/internal/http/restapi/operations"
+	"github.com/go-openapi/loads"
+	"github.com/jessevdk/go-flags"
+)
+
+func main() {
+	server()
+}
+
+func server() {
+	swaggerSpec, err := loads.Embedded(restapi.SwaggerJSON, restapi.FlatSwaggerJSON)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	api := operations.NewGitHubReleaseNotificationAPI(swaggerSpec)
+	server := restapi.NewServer(api)
+	server.ConfigureFlags() // inject API-specific custom flags. Must be called before args parsing
+
+	parser := flags.NewParser(server, flags.Default)
+	parser.ShortDescription = "GitHub Release Notification API"
+	parser.LongDescription = "GitHub Release Notification API that allows users to subscribe to email notifications about new releases of a chosen GitHub repository."
+
+	for _, optsGroup := range api.CommandLineOptionsGroups {
+		_, err := parser.AddGroup(optsGroup.ShortDescription, optsGroup.LongDescription, optsGroup.Options)
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}
+
+	if _, err := parser.Parse(); err != nil {
+		code := 1
+		fe := new(flags.Error)
+		if errors.As(err, &fe) {
+			if fe.Type == flags.ErrHelp {
+				code = 0
+			}
+		}
+		os.Exit(code)
+	}
+
+	server.ConfigureAPI() // configure handlers, routes and middleware
+
+	if err := server.Serve(); err != nil {
+		_ = server.Shutdown()
+
+		log.Fatalln(err)
+	}
+}
---
+++ internal/http/restapi/configure_git_hub_release_notification.go
@@ -0,0 +1,108 @@
+// This file is safe to edit. Once it exists it will not be overwritten
+
+package restapi
+
+import (
+	"crypto/tls"
+	"net/http"
+
+	"github.com/go-openapi/errors"
+	"github.com/go-openapi/runtime"
+	"github.com/go-openapi/runtime/middleware"
+
+	"github.com/dmytrovoron/github-release-notification/internal/http/restapi/operations"
+	"github.com/dmytrovoron/github-release-notification/internal/http/restapi/operations/subscription"
+)
+
+//go:generate go tool -modfile=../../../tools/go.mod github.com/go-swagger/go-swagger/cmd/swagger generate server --spec ../../../api/swagger.yaml --target ../ --name GitHubReleaseNotification --exclude-main
+
+func configureFlags(api *operations.GitHubReleaseNotificationAPI) {
+	// api.CommandLineOptionsGroups = []swag.CommandLineOptionsGroup{ ... }
+	_ = api
+}
+
+func configureAPI(api *operations.GitHubReleaseNotificationAPI) http.Handler {
+	// configure the api here
+	api.ServeError = errors.ServeError
+
+	// Set your custom logger if needed. Default one is log.Printf
+	// Expected interface func(string, ...any)
+	//
+	// Example:
+	// api.Logger = log.Printf
+
+	api.UseSwaggerUI()
+	// To continue using redoc as your UI, uncomment the following line
+	// api.UseRedoc()
+
+	api.JSONConsumer = runtime.JSONConsumer()
+	api.UrlformConsumer = runtime.DiscardConsumer
+
+	api.JSONProducer = runtime.JSONProducer()
+
+	// You may change here the memory limit for this multipart form parser. Below is the default (32 MB).
+	// subscription.SubscribeMaxParseMemory = 32 << 20
+
+	if api.SubscriptionConfirmSubscriptionHandler == nil {
+		api.SubscriptionConfirmSubscriptionHandler = subscription.ConfirmSubscriptionHandlerFunc(func(params subscription.ConfirmSubscriptionParams) middleware.Responder {
+			_ = params
+
+			return middleware.NotImplemented("operation subscription.ConfirmSubscription has not yet been implemented")
+		})
+	}
+	if api.SubscriptionGetSubscriptionsHandler == nil {
+		api.SubscriptionGetSubscriptionsHandler = subscription.GetSubscriptionsHandlerFunc(func(params subscription.GetSubscriptionsParams) middleware.Responder {
+			_ = params
+
+			return middleware.NotImplemented("operation subscription.GetSubscriptions has not yet been implemented")
+		})
+	}
+	if api.SubscriptionSubscribeHandler == nil {
+		api.SubscriptionSubscribeHandler = subscription.SubscribeHandlerFunc(func(params subscription.SubscribeParams) middleware.Responder {
+			_ = params
+
+			return middleware.NotImplemented("operation subscription.Subscribe has not yet been implemented")
+		})
+	}
+	if api.SubscriptionUnsubscribeHandler == nil {
+		api.SubscriptionUnsubscribeHandler = subscription.UnsubscribeHandlerFunc(func(params subscription.UnsubscribeParams) middleware.Responder {
+			_ = params
+
+			return middleware.NotImplemented("operation subscription.Unsubscribe has not yet been implemented")
+		})
+	}
+
+	api.PreServerShutdown = func() {}
+
+	api.ServerShutdown = func() {}
+
+	return setupGlobalMiddleware(api.Serve(setupMiddlewares))
+}
+
+// The TLS configuration before HTTPS server starts.
+func configureTLS(tlsConfig *tls.Config) {
+	// Make all necessary changes to the TLS configuration here.
+	_ = tlsConfig
+}
+
+// As soon as server is initialized but not run yet, this function will be called.
+// If you need to modify a config, store server instance to stop it individually later, this is the place.
+// This function can be called multiple times, depending on the number of serving schemes.
+// scheme value will be set accordingly: "http", "https" or "unix".
+func configureServer(server *http.Server, scheme, addr string) {
+	_ = server
+	_ = scheme
+	_ = addr
+}
+
+// The middleware configuration is for the handler executors. These do not apply to the swagger.json document.
+// The middleware executes after routing but before authentication, binding and validation.
+func setupMiddlewares(handler http.Handler) http.Handler {
+	return handler
+}
+
+// The middleware configuration happens before anything, this middleware also applies to serving the swagger.json document.
+// So this is a good place to plug in a panic handling middleware, logging and metrics.
+func setupGlobalMiddleware(handler http.Handler) http.Handler {
+	return handler
+}
